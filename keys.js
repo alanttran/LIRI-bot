5e9a6931023777665e75c06f562c63273a09ef8e
alanttran <mr.alan.tran@gmail.com>
2017-06-22 01:25:27 -0700

    most of assignment done

---
+++ keys.js
@@ -0,0 +1,8 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'XsPCnMtk2IVIQlnntCEpCBUw3',
+  consumer_secret: 'Nmb56qM6crXDmEgTy4Co0b8bbM6vHMQbM55E1wYloUl5WvUwLP',
+  access_token_key: '2975532325-TnR4YZCXFriONGTItA56EF4BNzZoqOYxqZhfzdJ',
+  access_token_secret: 'KesVmN0OF9n1fTPUiT5RNld3wP5rmfLMu9uY1MlZqV1su',
+}
--- liri.js
+++ liri.js
@@ -1,28 +1,88 @@
-var request = require("request");
-var fs = require("fs");
+"use strict";
+var request = require("request"); // request
+
+var fs = require("fs"); //file system
+
+var Twitter = require('twitter'); // twitter
+var keys = require("./keys.js"); // twitter keys
+var client = new Twitter(keys.twitterKeys);
+
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: '598e9540d9d54268af1b40a4cf5eb222',
+    secret: '628bcf05d39c443d82dc847e5c3969aa'
+});
 
 var nodeArgs = process.argv;
 
-var myFunction = process.argv[1];
+var myFunction = process.argv[2];
 
 var query = "";
 
-for (var i = 2; i < nodeArgs.length; i++) {
-	query += nodeArgs[i];
+for (var i = 3; i < nodeArgs.length; i++) {
+    query += nodeArgs[i] + " ";
 }
 
-if(myFunction === 'my-tweats'){
+if (myFunction === 'my-tweets') {
+    var params = { screen_name: 'origin' };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("\n" + (i + 1) + " - " + tweets[i].created_at.substring(0, 10) + "\n" + tweets[i].text + "\n");
+            }
+        } else {
+            console.log(error);
+        }
+    });
+} else if (myFunction === 'spotify-this-song') {
+    if (query.length === 0) {
+        query = 'The Sign';
+    }
 
-}
-else if( myFunction === 'spotify-this-song'){
+    spotify
+        .search({ type: 'track', query: query, limit: 1 })
+        .then(function(response) {
+            var artist = response.tracks.items[0].album.artists[0].name;
+            var previewURL = response.tracks.items[0].preview_url;
+            var name = response.tracks.items[0].name;
+            var album = response.tracks.items[0].album.name;
+            console.log("\n");
+            console.log("Name: " + name);
+            console.log("Artist(s): " + artist);
+            console.log("Album: " + album);
+            console.log("Preview URL: " + previewURL);
+            //console.log(JSON.stringify(response.tracks.items[0].preview_url, null, 2));
 
-}
-else if (myFunction === 'movie-this'){
+        })
+        .catch(function(err) {
+            console.log(error);
+        });
 
-}
-else if (myFunction === 'do-what-it-says'){
+} else if (myFunction === 'movie-this') {
+	if (query.length === 0) {
+        query = 'Mr. Nobody';
+    }
+    request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
-}
-else{
-	console.log('Invalid command');
+        if (!error && response.statusCode === 200) {
+        	console.log(JSON.stringify(body, null, 2));
+        	console.log("\n");
+        	console.log("Title: " + JSON.parse(body).Title);
+        	console.log("Year: " + JSON.parse(body).Year);
+        	console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        	console.log("Country: " + JSON.parse(body).Country);
+        	console.log("Language: " + JSON.parse(body).Language);
+        	console.log("Plot: " + JSON.parse(body).Plot);
+        	console.log("Actors: " + JSON.parse(body).Actors);
+        	console.log("Rotten Tomatoes URL: ???");
+        	
+            //console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+        }
+    });
+
+
+} else if (myFunction === 'do-what-it-says') {
+
+} else {
+    console.log('Invalid command');
 }
